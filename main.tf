db4d3f3cafc118d01ca297681681ac7351a48ba6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-01 11:01:31 +0530

    nat

--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@ resource "aws_subnet" "main" {
   count = length(var.cidr_block)
   cidr_block = var.availablity_zones[count.index]
   vpc_id = var.vpc_id
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-${var.name}-subnet-${count.index+1}" }
-  )
+  tags = merge(local.common_tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
 }
 
 resource "aws_route_table" "main" {
