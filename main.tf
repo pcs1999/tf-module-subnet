6e15b04d91cdd93c5a2a822f6605a72eed940702
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-05-02 13:05:52 +0530

    separate subnets creation

--- main.tf
+++ main.tf
@@ -1,36 +1,49 @@
-// creating the subnets
-resource "aws_subnet" "main" {
-  count = length(var.cidr_block)
-  cidr_block = var.cidr_block[count.index]
-  availability_zone = var.availability_zones[count.index]
-  vpc_id = var.vpc_id
-  tags = merge(local.common_tags, { Name = "${var.env}-${var.name}-subnet-${count.index + 1}" })
+module "subnets" {
+  source                    = "./subnets"
+  cidr_block                = var.cidr_block
+  env                       = var.env
+  availability_zones        = var.availability_zones
+  default_vpc_id            = var.default_vpc_id
+  name                      = var.name
+  vpc_id                    = var.vpc_id
+  vpc_peering_connection_id = var.vpc_peering_connection_id
 }
 
-// creating the routing table
-resource "aws_route_table" "route_table" {
-  vpc_id = var.vpc_id
-
-  route {
-    cidr_block        = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = var.vpc_peering_connection_id
-  }
-
-  tags = merge(local.common_tags, { Name = "${var.env}-${var.name}-route_table"})
-}
-
-// route table association to subnets
-resource "aws_route_table_association" "association" {
-  count = length(aws_subnet.main)
-  subnet_id      = aws_subnet.main.*.id[count.index]
-  route_table_id = aws_route_table.route_table.id
-}
-
-// creating a route to igw
-resource "aws_route" "gw_route" {
-  count = var.internet_gw ? 1 : 0
-  route_table_id = aws_route_table.route_table.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = var.internet_gw_id
-
-}
+#// creating a route to igw
+#resource "aws_route" "gw_route" {
+#  count = var.internet_gw ? 1 : 0
+#  route_table_id = aws_route_table.route_table.id
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id = var.internet_gw_id
+#
+#}
+#
+#//creating a INTERNET_GATEWAY
+#resource "aws_internet_gateway" "igw" {
+#  count = var.internet_gw ? 1 : 0
+#
+#  vpc_id = var.vpc_id
+#
+#  tags = merge (local.common_tags, { Name = "${var.env}-igw" } )
+#
+#}
+#
+#// creating the ELASTIC_IP
+#resource "aws_eip" "ngw-elastic" {
+#  vpc      = true
+#}
+#
+#// Creating the NAT_gateway
+#resource "aws_nat_gateway" "NATGW" {
+#  count = var.nat_gw ? 1 : 0
+#
+#  allocation_id = aws_eip.ngw-elastic.id
+#  subnet_id     = var.public_subnet_ids[0]
+#
+#  tags = merge (local.common_tags, { Name = "${var.env}-NAT_GW" } )
+#
+#
+#  # To ensure proper ordering, it is recommended to add an explicit dependency
+#  # on the Internet Gateway for the VPC.
+#  //depends_on = [aws_internet_gateway.igw]
+#}
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -3,8 +3,6 @@ variable "availability_zones" {}
 variable "env" {}
 variable "default_vpc_id" {}
 variable "vpc_id" {}
-variable "vpc_peering_connection_id" {}
-variable "internet_gw" {}
-variable "nat_gw" {}
 variable "name" {}
-//variable "internet_gw_id" {}
+variable "vpc_peering_connection_id" {}
+variable "tags" {}
